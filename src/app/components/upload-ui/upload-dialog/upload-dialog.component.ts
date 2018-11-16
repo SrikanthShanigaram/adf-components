43fb177ed455696c08ae9ad911aa78dbda0575ec
Srikanth <kanth.shanigaram@gmail.com>
2018-11-16 22:26:43 +0530

    Upload drag and drop and upload begin upload method to modify options

--- src/app/components/upload-ui/upload-dialog/upload-dialog.component.ts
+++ src/app/components/upload-ui/upload-dialog/upload-dialog.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core'
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import { UploadFilesEvent,ConfirmDialogComponent } from '@alfresco/adf-content-services';
+import { UploadFilesEvent } from '@alfresco/adf-content-services';
 import { FileModel } from '@alfresco/adf-core';
 import { MatDialog } from '@angular/material';
 
@@ -12,69 +12,73 @@ import { MatDialog } from '@angular/material';
 export class UploadDialogComponent implements OnInit{
 
     constructor(
-        public dialogRef: MatDialogRef<UploadDialogComponent>,public dialog: MatDialog,
+        public dialogRef: MatDialogRef<UploadDialogComponent>, public dialog: MatDialog,
         @Inject(MAT_DIALOG_DATA) public data: any) {}
     fiscalYear = [];
     fiscalMonth = [];
-    currentYear;
-    origin= [ 'Tax Advisor', 'Tax Advisor Client'];
-    service= [ 'Buchhaltung', 'Jahresabschluss', 'Lohn', 'Steuerberatung', 'Sonstiges'];
+    selectedYear;
+    selectedMonth;
+    selectedOrigin;
+    selectedService;
+    origin = [ 'Tax Advisor', 'Tax Advisor Client'];
+    service = [ 'Buchhaltung', 'Jahresabschluss', 'Lohn', 'Steuerberatung', 'Sonstiges'];
     ngOnInit() {
-        var date = new Date();
-        this.currentYear= date.getFullYear();
+        const date = new Date();
+        this.selectedYear = date.getFullYear();
         // setting from from last five year
-        var fiscalYearStart: number = this.currentYear-5;
-        var fiscalYearLimit: number =  10;
-        var index: number
-        for(index = fiscalYearStart; index<= fiscalYearStart+ fiscalYearLimit; index++){
+        const fiscalYearStart: number = this.selectedYear - 5;
+        const fiscalYearLimit =  10;
+        let index = 0
+        for ( index = fiscalYearStart; index <= fiscalYearStart + fiscalYearLimit; index++){
             this.fiscalYear.push(index);
         }
 
-        var fiscalMonthStart= 1 ;
-        var fiscalMonthlimit= 12;
-        for(index= fiscalMonthStart; index<= fiscalMonthlimit; index++){
+        const fiscalMonthStart = 1 ;
+        const fiscalMonthlimit = 12;
+        for ( index = fiscalMonthStart; index <= fiscalMonthlimit; index++){
             this.fiscalMonth.push(index);
         }
-        console.log(this.currentYear,"fiuscalyear")
     }
     
-    onNoClick(): void {
-       this.dialogRef.close();
-    }
     onFileUploaded(event: any) {
-        console.log(event,"event");
+        console.log(event, "event");
     }
     onBeforeFileUpload(event: UploadFilesEvent) {
         event.pauseUpload();
         const files = event.files || [];
-        console.log("Test :: ",files)
-        var fileIndex = 0
-        for(; fileIndex < files.length; fileIndex++){
-            var fileModel: FileModel = files[fileIndex];
-            console.log(fileIndex,fileModel);
-            const dialogRef = this.dialog.open(ConfirmDialogComponent, {
-                data: {
-                    title: 'Upload',
-                    message: `Are you sure you want to upload ${files.length} file(s)?`
-                },
-                minWidth: '250px'
-            });
+        let fileIndex = 0
+        for (; fileIndex < files.length; fileIndex++){
+            const fileModel: FileModel = files[fileIndex];
+            console.log(fileIndex, fileModel);
+            // const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+            //     data: {
+            //         title: 'Upload',
+            //         message: `Are you sure you want to upload ${files.length} file(s)?`
+            //     },
+            //     minWidth: '250px'
+            // });
 
-            dialogRef.afterClosed().subscribe(result => {
-                if (result === true) {
-                    console.log("==",fileModel);
-                    if( fileModel.options == null ){
-                        fileModel.options = {}
-                    }
-                    if( fileModel.options.properties == null ){
-                        fileModel.options.properties = {}
-                    }
-                    // Getting Issue becuase og no qname with bt [No content Model]        
-                    fileModel.options.properties['bt:fiscalYear'] = 2018;
-                    console.log(fileIndex,fileModel);
-                    event.resumeUpload();
-                }
-            });           
+            // dialogRef.afterClosed().subscribe(result => {
+            //     if (result === true) {
+            //         console.log("==", fileModel);                    
+            //     }
+            // });  
+            if ( fileModel.options == null ){
+                fileModel.options = {}
+            }
+            // Setting document type
+            fileModel.options['nodeType'] = 'bt:finacialDocument';
+            if ( fileModel.options.properties == null ){
+                fileModel.options.properties = {}
+            }
+            // Setting custom model content types
+            fileModel.options.properties['bt:fiscalYear'] = this.selectedYear;
+            fileModel.options.properties['bt:fiscalMonth'] = this.selectedMonth;
+            fileModel.options.properties['bt:origin'] = this.selectedOrigin;
+            fileModel.options.properties['bt:service'] = this.selectedService;
+            console.log(fileIndex, fileModel);         
         }
+        this.dialogRef.close();
+        event.resumeUpload();
     }
 }
